167c010105aca22646eed0940880aa9abeef2382
Bryan <bryan.l.bergo@gmail.com>
2026-02-06 20:00:12 -0800

    feat: contributor page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,13 @@ import 'nextra-theme-docs/style.css'
 import { ReactNode } from 'react'
 
 export const metadata = {
-  // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
 const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
-  // ... Your additional navbar options
+    logo={<b>Mobility Scooter Project Docs</b>}
   />
 )
 const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
@@ -29,9 +27,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
       suppressHydrationWarning
     >
       <Head
-      // ... Your additional head options
       >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
       <body>
         <Layout
@@ -40,7 +36,6 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
           footer={footer}
-        // ... Your additional layout options
         >
           {children}
         </Layout>
